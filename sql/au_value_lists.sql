b3b90f711f9c690851623c14d715c41ff13e4782
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2023-04-17 16:07:21 +0200

    Modifications pour schéma au

---
+++ sql/au_value_lists.sql
@@ -0,0 +1,27 @@
+DROP TYPE IF EXISTS administrative_hierarchy_level_type_value;
+CREATE TYPE administrative_hierarchy_level_type_value AS ENUM (
+'1st_order', 
+'2nd_order',
+'3rd_order',
+'4th_order',
+'5th_order',
+'6th_order');
+
+DROP TYPE IF EXISTS legal_status_value;
+CREATE TYPE legal_status_value AS ENUM (
+'agreed', 
+'not_agreed');
+
+DROP TYPE IF EXISTS technical_status_value;
+CREATE TYPE technical_status_value AS ENUM (
+'edge_matched', 
+'calculated',
+'not_edge_matched');
+
+DROP TYPE IF EXISTS land_cover_type_value;
+CREATE TYPE land_cover_type_value AS ENUM (
+'land_area', 
+'coastal_water',
+'inland_water');
+
+
