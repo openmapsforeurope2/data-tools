43badfecde7951e9fc87697c38e8494aab848d11
pjjmunier <pierre.munier@ign.fr>
2023-07-23 17:58:05 +0200

    extract fr

--- script/extract_fr.sh
+++ script/extract_fr.sh
@@ -2,7 +2,8 @@
 
 echo $(date)
 
-python3 border_extraction.py -c conf.json -T tn -t road_link -b be -d 3000 fr
+python3 border_extraction.py -c conf.json -T tn -t road_link -b false -B international -d 3000 fr
+python3 border_extraction.py -c conf.json -T tn -t road_link -b be -d 3000 -n fr
 python3 border_extraction.py -c conf.json -T tn -t road_link -b lu -d 3000 -n fr
 python3 border_extraction.py -c conf.json -T tn -t road_link -b de -d 3000 -n fr
 python3 border_extraction.py -c conf.json -T tn -t road_link -b ch -d 3000 -n fr
