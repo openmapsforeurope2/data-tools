1ec782f47399d51489cb10cfb589fb627ef96ba8
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2023-11-24 16:20:58 +0100

    Update fichier init_tables.sh

--- init_tables.sh
+++ init_tables.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 python3 script/table_creation.py -c conf_template.json -m mcd.json -T au
 python3 script/table_creation.py -c conf_template.json -m mcd.json -T hy
 python3 script/table_creation.py -c conf_template.json -m mcd.json -T ib
