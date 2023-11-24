458a204e103ed02b5558cee392a152c48d772fa7
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2023-11-24 16:14:09 +0100

    Différenciation conf normal et conf template

--- init_tables.bat
+++ init_tables.bat
@@ -1,4 +1,4 @@
-python3 script/table_creation.py -c conf.json -m mcd.json -T au
-python3 script/table_creation.py -c conf.json -m mcd.json -T hy
-python3 script/table_creation.py -c conf.json -m mcd.json -T ib
-python3 script/table_creation.py -c conf.json -m mcd.json -T tn
+python3 script/table_creation.py -c conf_template.json -m mcd.json -T au
+python3 script/table_creation.py -c conf_template.json -m mcd.json -T hy
+python3 script/table_creation.py -c conf_template.json -m mcd.json -T ib
+python3 script/table_creation.py -c conf_template.json -m mcd.json -T tn
