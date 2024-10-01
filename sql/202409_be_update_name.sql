314fc5987b7e83557b7438ab12fc63e405f8b14a
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2024-10-01 08:49:23 +0200

    New script to update road_link and railway_link for NL based on new information provided by KNL.

--- sql/202409_be_update_name.sql
+++ sql/202409_be_update_name.sql
@@ -70,7 +70,7 @@ SET name =
 
 
 -- au.administrative_unit_area_3
-UPDATE au.administrative_unit_area_2 a
+UPDATE au.administrative_unit_area_3 a
 SET name = 
     CASE 
     WHEN b.namelocal = b.namefre THEN
@@ -135,6 +135,169 @@ SET name =
             )
       ELSE a.name
       END
-    FROM au.administrative_unit_area_2, province b
+    FROM au.administrative_unit_area_3, province b
     WHERE a.country = 'be' AND a.national_code = b.niscode 
 ;
+
+
+-- au.administrative_unit_area_4
+UPDATE au.administrative_unit_area_4 a
+SET name = 
+    CASE 
+    WHEN b.namelocal = b.namefre THEN
+        jsonb_build_array(                                                         
+            jsonb_build_object(
+                'script', 'latn',
+                'display', '1'::INT,
+                'language', 'fre',       
+                'spelling', b.namefre,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namefre
+            ),
+            jsonb_build_object(     
+                'script', 'latn',
+                'display', '2'::INT,
+                'language', 'dut',
+                'spelling', b.namedut,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namedut
+            ),
+            jsonb_build_object(                                            
+                'script', 'latn',
+                'display', '3'::INT,
+                'language', 'ger',
+                'spelling', b.nameger,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.nameger
+                )
+            )
+     WHEN b.namelocal = b.namedut THEN
+        jsonb_build_array(                                                         
+            jsonb_build_object(
+                'script', 'latn',
+                'display', '1'::INT,
+                'language', 'dut',       
+                'spelling', b.namedut,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namedut
+            ),
+            jsonb_build_object(     
+                'script', 'latn',
+                'display', '2'::INT,
+                'language', 'fre',
+                'spelling', b.namefre,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namefre
+            ),
+            jsonb_build_object(                                            
+                'script', 'latn',
+                'display', '3'::INT,
+                'language', 'ger',
+                'spelling', b.nameger,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.nameger
+                )
+            )
+      ELSE a.name
+      END
+    FROM au.administrative_unit_area_4, arrondissement b
+    WHERE a.country = 'be' AND a.national_code = b.niscode 
+;
+
+-- au.administrative_unit_area_5
+UPDATE au.administrative_unit_area_5 a
+SET name = 
+    CASE 
+    WHEN b.namelocal = b.namefre THEN
+        jsonb_build_array(                                                         
+            jsonb_build_object(
+                'script', 'latn',
+                'display', '1'::INT,
+                'language', 'fre',       
+                'spelling', b.namefre,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namefre
+            ),
+            jsonb_build_object(     
+                'script', 'latn',
+                'display', '2'::INT,
+                'language', 'dut',
+                'spelling', b.namedut,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namedut
+            ),
+            jsonb_build_object(                                            
+                'script', 'latn',
+                'display', '3'::INT,
+                'language', 'ger',
+                'spelling', b.nameger,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.nameger
+                )
+            )
+     WHEN b.namelocal = b.namedut THEN
+        jsonb_build_array(                                                         
+            jsonb_build_object(
+                'script', 'latn',
+                'display', '1'::INT,
+                'language', 'dut',       
+                'spelling', b.namedut,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namedut
+            ),
+            jsonb_build_object(     
+                'script', 'latn',
+                'display', '2'::INT,
+                'language', 'fre',
+                'spelling', b.namefre,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.namefre
+            ),
+            jsonb_build_object(                                            
+                'script', 'latn',
+                'display', '3'::INT,
+                'language', 'ger',
+                'spelling', b.nameger,
+                'nativeness', 'endonym',
+                'name_status', 'official',
+                'spelling_latn', b.nameger
+                )
+            )
+      ELSE a.name
+      END
+    FROM au.administrative_unit_area_5, municipality b
+    WHERE a.country = 'be' AND a.national_code = b.niscode 
+;
+
+-- Update label field
+
+UPDATE au.administrative_unit_area_2 a
+SET label = b.namelocal
+FROM region b
+WHERE a.country = 'be' AND a.national_code = b.niscode ;
+
+UPDATE au.administrative_unit_area_3 a
+SET label = b.namelocal
+FROM province b
+WHERE a.country = 'be' AND a.national_code = b.niscode ;
+
+UPDATE au.administrative_unit_area_4 a
+SET label = b.namelocal
+FROM arrondissement b
+WHERE a.country = 'be' AND a.national_code = b.niscode ;
+
+UPDATE au.administrative_unit_area_5 a
+SET label = b.namelocal
+FROM municipality b
+WHERE a.country = 'be' AND a.national_code = b.niscode ;
