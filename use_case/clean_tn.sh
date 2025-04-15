05012fc02ad34225ffdc095bc26ba133fe683fc9
Noémie Grémeaux <noemie.gremeaux@ign.fr>
2025-04-15 11:33:08 +0200

    Update clean_tn.sh

--- use_case/clean_tn.sh
+++ use_case/clean_tn.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
 cd ..
+# AT all TN layers, country by country (02/04/2025)
+python3 script/border_extract.py -c conf_v6.json -T tn -b de -d 1000 at
+python3 script/border_extract.py -c conf_v6.json -T tn -b ch -d 1000 -n at
+python3 script/border_extract.py -c conf_v6.json -T tn -b cz -d 1000 -n at
+python3 script/border_extract.py -c conf_v6.json -T tn -b hu -d 1000 -n at
+python3 script/border_extract.py -c conf_v6.json -T tn -b it -d 1000 -n at
+python3 script/border_extract.py -c conf_v6.json -T tn -b li -d 1000 -n at
+python3 script/border_extract.py -c conf_v6.json -T tn -b si -d 1000 -n at
+python3 script/border_extract.py -c conf_v6.json -T tn -b sk -d 1000 -n at
+python3 script/clean.py -c conf_v6.json -d 5 -T tn at
+python3 script/integrate.py -c conf_v6.json -T tn -s 10
+
 # BE road_link only
 python3 data-tools/script/border_extract.py -c conf.json -T tn -t road_link -d 1000 be '#'
 python3 data-tools/script/clean.py -c conf.json -d 5 -T tn -t road_link_w be
 python3 data-tools/script/integrate.py -c conf.json -T tn -t road_link -s 10
+
+# CZ all TN layers
+python3 script/border_extract.py -c conf_v6.json -T tn -d 1000 cz #
+python3 script/clean.py -c conf_v6.json -d 5 -T tn cz
+python3 script/integrate.py -c conf_v6.json -T tn -s 10
 
 # FR (trop lourd pour faire toutes les frontières d'un coup) road_link only
 #python3 data-tools/script/border_extract.py -c conf.json -T tn -t road_link -d 1000 fr '#'
